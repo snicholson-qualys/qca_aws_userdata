ad2dab61e751d0e76fd1e86355c1d8679db011b3
Sean Nicholson <snicholson@qualys.com>
2018-11-02 12:45:43 -0400

    v1.0.1
    Added ubuntu/debian script

--- deploy_qualys_debian.sh
+++ deploy_qualys_debian.sh
@@ -6,10 +6,12 @@ outputFile=""
 amzFile=""
 bucket=""
 resource="s3://${bucket}/${amzFile}"
-yum -y install jq
+apt update >> /dev/null
+apt -y install jq >> /dev/null
+apt -y install awscli >> /dev/null
 export AWS_ACCESS_KEY_ID=$(/usr/bin/curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/${awsRoleName}/AccessKeyId | jq -r '.AccessKeyId')
 export AWS_SECRET_ACCESS_KEY=$(/usr/bin/curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/${awsRoleName}/AccessKeyId | jq -r '.SecretAccessKey')
 export AWS_SESSION_TOKEN=$(/usr/bin/curl --silent http://169.254.169.254/latest/meta-data/iam/security-credentials/${awsRoleName}/AccessKeyId | jq -r '.Token')
 aws s3 cp ${resource} ${outputFile}
-rpm -ivh ${outputFile}
+dpkg --install ${outputFile}
 /usr/local/qualys/cloud-agent/bin/qualys-cloud-agent.sh ActivationId=${activationId} CustomerId=${customerID}
